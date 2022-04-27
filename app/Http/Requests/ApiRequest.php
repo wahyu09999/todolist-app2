e00be5aa7b28b7b225c744ce2df0c87923c29165
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-04-27 12:57:27 +0700

    init

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+    
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+}
